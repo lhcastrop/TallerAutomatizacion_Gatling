79cfef09e9b7f577d91bb7e51b4d3ea2233dd386
Leonardo Castro <44737382+lhcastrop@users.noreply.github.com>
2025-06-30 11:47:56 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -53,7 +53,7 @@ class LoginTest extends Simulation{
   // 5. Ejecución de escenarios
   setUp(
     //scn.inject(rampUsers(10).during(10)),
-    validLogin.inject(atOnceUsers(50)),
+    scn.inject(atOnceUsers(50)),
     invalidCredentialsLogin.inject(rampUsers(10).during(10)),
     invalidEmailFormat.inject(rampUsers(10).during(10))
   ).protocols(httpConf)
